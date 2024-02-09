ce8828637223ae4f426328233ef1af57560dab57
ChrisCh9 <151587068+ChrisCh9@users.noreply.github.com>
2024-02-10 00:19:00 +0200

    To be done
    To be done

---
+++ 9th-Set-Day-9/Exercise70 (1).py
@@ -0,0 +1,7 @@
+#Display a random integer between 1 and 100 inclusive. 
+
+from random import randint
+
+num = randint(1,100)
+
+print(num)
---
+++ 9th-Set-Day-9/Exercise72 (2).py
@@ -0,0 +1,3 @@
+#Display a random fruit from a list of five fruits. 
+
+import
